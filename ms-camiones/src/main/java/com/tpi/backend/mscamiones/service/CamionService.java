a4ad9b77fe083b8326869f399b9179fdcab33360
Santini Agustin <87019@sistemas.frc.utn.edu.ar>
2025-11-13 17:09:04 -0300

    Agregado de endpoint GET

--- ms-camiones/src/main/java/com/tpi/backend/mscamiones/service/CamionService.java
+++ ms-camiones/src/main/java/com/tpi/backend/mscamiones/service/CamionService.java
@@ -3,6 +3,8 @@ package com.tpi.backend.mscamiones.service;
 import com.tpi.backend.mscamiones.dto.CamionDto;
 import com.tpi.backend.mscamiones.model.Camion;
 
+import java.util.List;
+
 public interface CamionService {
     
     /**
@@ -12,4 +14,11 @@ public interface CamionService {
      * @throws jakarta.persistence.EntityNotFoundException si el transportistaId no existe.
      */
     Camion guardarCamion(CamionDto dto);
+
+    /**
+     * Lista todos los camiones o los filtra por estado.
+     * @param estado El estado por el cual filtrar ("libre" u "ocupado"). Si es nulo, lista todos.
+     * @return Una lista de camiones.
+     */
+    List<Camion> listar(String estado);
 }
